c2c0c0b4223059a4eb32b31f06c8d98f448d6365
manikanat05411 <ganeshkedari685@gmail.com>
2026-08-12 11:42:03 +0530

    This is a sample code for testing

---
+++ src/firstjava/corejava2.java
@@ -0,0 +1,14 @@
+package firstjava;
+
+public class corejava2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test for Git");
+		System.out.println("This is a test for Git");
+		System.out.println("This is a test for Git");
+		System.out.println("This is a test for Git");
+
+
+	}
+
+}
